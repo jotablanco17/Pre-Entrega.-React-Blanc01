fdb7f1598eb307c585b91d4933832a0ccc818616
Juan Blanco <jotablancoyt@gmail.com>
2023-08-24 20:19:40 -0300

    entrega

---
+++ blanco-react/src/componentes/Cartwidget/Cartwidget.jsx
@@ -0,0 +1,11 @@
+import './cart.css'
+
+const Cartwidget = () => {
+    return (
+        <div>
+            < img className='carta' src="https://i.pinimg.com/736x/28/56/e9/2856e9c85b05527f72ad05fc105cf6a7.jpg" alt="" />
+        </div>
+    )
+}
+
+export default Cartwidget
